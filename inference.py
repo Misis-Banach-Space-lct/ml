80d59231b0d7fc70bef047c793234d45b93923c6
Timur <tz220404@gmail.com>
2023-11-07 16:21:50 +0300

    changed checking unique id

--- inference.py
+++ inference.py
@@ -1,10 +1,13 @@
 import cv2
 
-def get_frame_imgs_from_video(model, video_path):
+def get_frame_imgs_from_video(model, video_path: str):
     cap = cv2.VideoCapture(video_path)
     fps = cap.get(cv2.CAP_PROP_FPS)
     frame_cnt = cap.get(cv2.CAP_PROP_FRAME_COUNT)
     duration = frame_cnt/fps
+
+    about_frames = []
+    saves = []
 
     frame_counter = 1
     while cap.isOpened():
@@ -21,10 +24,25 @@ def get_frame_imgs_from_video(model, video_path):
                     if cls_ids[i] != 3:
                         cls_name = clss[cls_ids[i]]
                         xyxy = data.boxes.numpy().xyxy[i]
-                        image = cv2.rectangle(frame, (int(xyxy[0]), int(xyxy[1])),
-                                              (int(xyxy[2]), int(xyxy[3])), (0, 255, 0), 2)
+                        image = cv2.rectangle(frame,
+                                              (int(xyxy[0]), int(xyxy[1])),
+                                              (int(xyxy[2]), int(xyxy[3])),
+                                              (0, 0, 255), 2)
 
-                        cv2.imwrite('./output/'+cls_name + f'_{int(tracked_ids[i])}_{int(confs[i]*100)}_{round(frame_counter * (duration/frame_cnt), 3)}.jpg', image)
+                        frame_time = round(frame_counter * (duration/frame_cnt), 3)
+                        tracked_id = int(tracked_ids[i])
+                        if (tracked_id not in saves):
+                            cv2.imwrite('./output/'+cls_name +
+                                        f'_{tracked_id}_{int(confs[i]*100)}_{frame_time}.jpg',
+                                        image)
+                            saves.append(tracked_id)
+                        about_frames.append({
+                            "name": cls_name,
+                            "tracked_id": tracked_ids[i],
+                            "conf": confs[i],
+                            "frame_time": frame_time
+                        })
             frame_counter += 1
         else:
-            break
+            return about_frames
+
--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from ultralytics import YOLO
+from ultralytics import YOLO, RTDETR
 from inference import get_frame_imgs_from_video
 
 if __name__ == "__main__":
-    model = YOLO('weights/best_yolov8l.pt')
+    model = YOLO('weights/best_yolov8l.pt', task='detect')
+    # model = RTDETR('weights/best_rtdetrl.pt')
 
-    video_path = ["videos/flowers.mp4", "videos/video_2023-11-06_15-31-43.mp4"]
+    video_path = ["videos/flowers.mp4", "videos/video_2023-11-06_15-31-43.mp4", "videos/balloons.mp4"]
     photo_path = "photos/photo_2023-11-06_14-08-42.jpg"
-    get_frame_imgs_from_video(model, video_path[1])
+    about_frames = get_frame_imgs_from_video(model, video_path[2])
+
+    print(about_frames)
+
