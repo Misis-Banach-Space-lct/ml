d146213f6292458aecf1405871cf851c7031314a
marulyanova <93385722+marulyanova@users.noreply.github.com>
2023-11-07 22:20:55 +0300

    Update save_cards.py

--- save_cards.py
+++ save_cards.py
@@ -1,15 +1,20 @@
-model = YOLO('/content/drive/MyDrive/best.pt')
+model = YOLO('/content/drive/MyDrive/best.pt') # YOLO от 5 ноября, 11 версия датасета
 
 import torch
 frames = []
 
 with torch.no_grad():
-  results = model.track(source='/content/drive/MyDrive/Копия 210.VC9.8.23 Набережная (Пароход) 2023-09-15 18-00-00_000+0300 [5m0s] (1).mp4',
-                        save=True, stream=True, tracker = "bytetrack.yaml", vid_stride = 5)
+  results = model.track(source='/content/drive/MyDrive/Копия 53.VC3.2.6 Белая Набережная 2023-09-20 20-52-40_000+0300 [3m0s].mp4',
+                        save=True, stream=True, tracker = "bytetrack.yaml", vid_stride = 20)  # Частоту регулируем
   for res in results:
     frames.append(res)
 
+from ultralytics import RTDETR
+model_predictor = RTDETR('/content/drive/MyDrive/best_rtdetrl.pt') # detr обученный на 12 версии 
+
 import cv2
+
+names = {0: 'animal', 1: 'balloon', 2: 'cart', 3: 'person'}
 
 class Object():
   def __init__(self, id, cls):
@@ -17,9 +22,18 @@ class Object():
     self.cls = cls
     self.cnt = 0
     self.start = 0
+    self.path = ''
+    
+def process_cadr(result_model_predictor):
+    coords = []
+    res = result_model_predictor[0].boxes
+    for obj in res.data:
+        if int(obj[-1]) == 3 or int(obj[-1]) == 0: continue
+        if obj[-2] > 0.6:
+          coords.append(obj[:4])
+    return coords
 
-
-def save_cadrs(result_after_track):
+def save_cadrs(result_after_track, model_predictor):
     res = result_after_track
     objects = {}
 
@@ -27,31 +41,37 @@ def save_cadrs(result_after_track):
     for cadr in res:
       num_cadr += 1
       for obj in cadr.boxes.data:
-        if obj[-1] == 3:
-          continue
+        if int(obj[-1]) == 3 or int(obj[-1]) == 0: continue
 
         id = float(obj[4])
         if not id in objects.keys():
-            objects[id] = Object(id, float(obj[-1]))
+            objects[id] = Object(id, int(obj[-1]))
             objects[id].start = num_cadr
 
         if objects[id].cnt < 3:
             objects[id].cnt += 1
             image = cadr.orig_img.copy()
 
-            x1 = int(max(obj[0] - 100, 0))
-            x2 = int(min(obj[2] + 100, cadr.orig_shape[1]))
-            y1 = int(max(obj[1] - 100, 0))
-            y2 = int(min(obj[3] + 100, cadr.orig_shape[0]))
+            x1 = int(max(obj[0] - 50, 0))
+            x2 = int(min(obj[2] + 50, cadr.orig_shape[1]))
+            y1 = int(max(obj[1] - 50, 0))
+            y2 = int(min(obj[3] + 50, cadr.orig_shape[0]))
 
             crop_img = image[y1 : y2, x1 : x2]
-            cv2.imwrite('./output/' + str(round(id, 4)) + '_' + str(int(objects[id].cnt)) + '_' + str(int(objects[id].cls)) +'.jpg', crop_img)
-    return objects
+            coordinates = process_cadr(model_predictor.predict(source = crop_img))
 
-saved = save_cadrs(frames)
+            if len(coordinates) > 0:
+              for coodninate in coordinates:
+                  cv2.rectangle(crop_img, (int(coodninate[0]), int(coodninate[1])), (int(coodninate[2]), int(coodninate[3])), (0, 0, 255), 2)
+                  cv2.putText(crop_img, names[objects[id].cls], (int(coodninate[0]), int(coodninate[1]) + 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36, 255 , 12), 2)
+              image[y1 : y2, x1 : x2] = crop_img
+              cv2.imwrite('./cadr3/' + str(num_cadr) + '.jpg', image)
+              objects[id].path = str(num_cadr) + '.jpg'
 
-model2 = '...'
-import os
-for file in os.listdir('/content/output'):
-  if file[-3:] != 'jpg': continue
-  model2.predict(source = '/content/output/' + file, save = True)
+    cadrs = []
+    for _, obj in objects.items():
+        if obj.path != '':
+          cadrs.append(obj)
+    return cadrs
+
+saved = save_cadrs(frames, model_predictor)
