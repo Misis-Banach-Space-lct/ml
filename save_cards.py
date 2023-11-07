00f6dc074546e0fce5dda0d091f7db23d2cd63f6
marulyanova <93385722+marulyanova@users.noreply.github.com>
2023-11-07 23:28:00 +0300

    Update save_cards.py

--- save_cards.py
+++ save_cards.py
@@ -24,12 +24,12 @@ class Object():
     self.start = 0
     self.path = ''
     
-def process_cadr(result_model_predictor):
+def process_cadr(result_model_predictor, start_conf):
     coords = []
     res = result_model_predictor[0].boxes
     for obj in res.data:
         if int(obj[-1]) == 3 or int(obj[-1]) == 0: continue
-        if obj[-2] > 0.6:
+        if (obj[-2] + start_conf) / 2 > 0.6:
           coords.append(obj[:4])
     return coords
 
@@ -58,7 +58,7 @@ def save_cadrs(result_after_track, model_predictor):
             y2 = int(min(obj[3] + 50, cadr.orig_shape[0]))
 
             crop_img = image[y1 : y2, x1 : x2]
-            coordinates = process_cadr(model_predictor.predict(source = crop_img))
+            coordinates = process_cadr(model_predictor.predict(source = crop_img), obj[-2])
 
             if len(coordinates) > 0:
               for coodninate in coordinates:
